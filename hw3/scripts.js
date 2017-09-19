6d70258d563ad78d879476f61cb9e8cb6f973ded
tamarticus <tp1153@nyu.edu>
2017-09-19 01:00:20 -0400

    step 2

--- hw3/scripts.js
+++ hw3/scripts.js
@@ -1,6 +1,10 @@
 let userInput;
 userInput = prompt("Please enter a number", "Type your answer here.");
 for (let i = 1; i <= userInput; i = i +1){
-  $("#response").append("<br />" + i);
+  if (i%2==1){
+    $("#response").append("<br />" + i + " is odd");
+  } else{
+    $("#response").append("<br />" + i + " is even");
+  }
 
 }
