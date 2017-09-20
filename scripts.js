cdd89eeea74eee899c74b347c57f5ac3b8fa10e6
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-09-19 21:26:39 -0400

    Set up for homework

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// 1. Write a program so that when you reload your page, it asks for a number and prints all the numbers from 1 to it, including it.
+
+// 2. Building on that program, create it so that instead of printing the number, it prints something like:
+
+// This is the #response div.
+// 1 is odd.
+// 2 is even.
+// 3 is odd.
+
+// 3. Building on the previous program, have it print the same, except without that ugly “This is the #response div.” line.
+
+// 4. Write down what seem to be common mistakes you are making. Are you forgetting to add some aspect of the JavaScript syntax?
