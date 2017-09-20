80d74d1f045e787fdbba6973fed0f498bb1c740b
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-09-20 16:34:27 -0400

    Finish homework 3.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,40 @@
-// 1. Write a program so that when you reload your page, it asks for a number and prints all the numbers from 1 to it, including it.
+// 1. Write a program so that when you reload your page, 
+// it asks for a number and prints all the numbers from 1 to it, including it.
 
-// 2. Building on that program, create it so that instead of printing the number, it prints something like:
+let input;
+input = prompt("Enter your input");
+
+for (let i = 1 ; i <= input ; i = i + 1){
+  $("#response").append(i + "<br>");
+}
+
+// 2. Building on that program, create it so that instead of printing the number, 
+// it prints something like:
 
 // This is the #response div.
 // 1 is odd.
 // 2 is even.
 // 3 is odd.
 
-// 3. Building on the previous program, have it print the same, except without that ugly “This is the #response div.” line.
+// for (let i = 1 ; i <= input ; i = i + 1){
+//   if (i % 2 === 0){
+//     $("#response").append(i + " is even.<br>");
+//   } else {
+//     $("#response").append(i + " is odd.<br>");
+//   }
+// }
 
-// 4. Write down what seem to be common mistakes you are making. Are you forgetting to add some aspect of the JavaScript syntax?
+
+// $("#response").append();
+
+// 3. Building on the previous program, have it print the same, 
+// except without that ugly “This is the #response div.” line.
+
+// $("#response").html("");
+// for (let i = 1 ; i <= input ; i = i + 1){
+//   if (i % 2 === 0){
+//     $("#response").append(i + " is even.<br>");
+//   } if (i % !== 0) {
+//     $("#response").append(i + " is odd.<br>");
+//   }
+// }
