f3ae68d21521b1fedfe17cbf25ef6b0aa83957af
tamarticus <tp1153@nyu.edu>
2017-10-31 18:49:08 -0400

    can't get the 18 lines to appear

--- hw9/prologue.js
+++ hw9/prologue.js
@@ -1,29 +1,36 @@
-$("#prologue").html("<p>The text of the Prologue will go here.</p>");
+// 1. Set the content of #glosses.
 $("#glosses").html("<p>The glosses will go here.</p>");
-
-$("#glosses").html("<p>The glosses will go here.</p>");
-let line1, line1Text; // don’t need the intermediate step of line1TextArray
-line1 = [{text: "Whan", modern: "When"}, {text: "that"}, {text: "Aprill,",
-        modern: "April,"}, {text: "with"}, {text: "his"}, {text: "shoures",
-        modern: "showers"}, {text: "soote", modern: "sweet"}];
-// Create a blank string that opens two tags.
-line1Text = "<blockquote><p>";
-line1.forEach(function(word){
-  let wordString;
-  wordString = word.text;
-  if (word.modern){
-    // Add word.modern as a data attribute to the <a> tag.
-    wordString = "<a href='#' data-modern='" + word.modern + "'>" + wordString + "</a>";
-  }
-  line1Text = line1Text + wordString + " ";
-});
-// Break the line and close the two tags.
-line1Text = line1Text + "<br />(line 2 would go here)</p></blockquote>";
-$("#prologue").html(line1Text);
+// 2. Set the content of #prologue.
+$.getJSON("prologue.json", function(data){ // Note the data variable!
+  let prologueText; // Define the variable you didn’t need before.
+  prologueText = "<blockquote><p>"; // Open the tags.
+  // Now you can iterate over the data variable’s .lines property:
+  data.lines.forEach(function(line){ // We get a variable, line.
+    // Define a blank lineText.
+    let lineText;
+    lineText = "";
+    // Now iterate over each line. This part should be familiar.
+    line.forEach(function(word){
+      let wordString;
+      wordString = word.text;
+      if (word.modern){
+        wordString = "<a href='#' data-modern='" + word.modern + "'>" + wordString + "</a>";
+      }
+      lineText = lineText + wordString + " ";
+    });
+    // Add lineText with a line break to the prologueText.
+    prologueText = prologueText + lineText + "<br/>";
+  });
+  // Close the prologueText tags.
+  prologueText = prologueText + "</p></blockquote>";
+  // Replace the content of #prologue.
+  $("#prologue").html(prologueText);
+}); // Close the callback function & method.
+// 3. Wait around for the user to click on an <a> tag inside #prologue
+// and then change the content of #glosses.
 $("#prologue a").click(function(){
   let glossText, clickedWord, modernWord;
   clickedWord = $( this ).text();
-  // .data("modern") looks for the data-modern HTML attribute.
   modernWord = $( this ).data("modern");
   glossText = "<h2>You clicked on " + clickedWord + ", which means " + modernWord +"</h2>";
   $("#glosses").html(glossText);
