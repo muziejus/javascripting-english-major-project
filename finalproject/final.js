011cfc67c9b174ef78982ee31ea7294388630cae
tamarticus <tp1153@nyu.edu>
2017-12-20 22:19:17 -0500

    things are happening!

--- finalproject/final.js
+++ finalproject/final.js
@@ -4,7 +4,106 @@ tileLayer =
 L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
       attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://carto.com/attribution'>CARTO</a>",
       subdomains: "abcd",
-      maxZoom: 18
+      maxZoom: 200,
+
     });
 tileLayer.addTo(firstMap);
-firstMap.setView([40.73357, -73.97659],16);
+firstMap.setView([14.66667,-1.0],2);
+function changeView(
+){
+
+firstMap.setView([40.71427,-74.00597,],10);
+}
+function changeView2(
+){
+
+firstMap.setView([14.66667,-1.0],2);
+}
+let geoFeatures;
+$.getJSON("https://tamarticus.github.io/javascripting-english-major-project/finalproject/anotherbk.geo.json", function(data){
+  // Define the Leaflet layer.
+  let geoLayer;
+  // Iterate over the .features property of the GeoJSON object to
+  // create an array of objects (features), with every object’s
+  // properties as noted.
+  geoFeatures = data.features.map(function(feature){
+    return {
+      name: feature.properties.text,
+      category: feature.properties.special,
+      html: feature.properties.html,
+      tab: feature.properties.tab,
+      mentions: feature.properties.mentions,
+      lines: feature.properties.lines,
+      wikipedia: feature.properties.wikipedia,
+      latLng: L.latLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0])
+    };
+  });
+
+  geoLayer = L.featureGroup(geoFeatures.map(function(feature){
+let whichIcon;
+let popupContent;
+let category = feature.category;
+popupContent = feature.name;
+console.log(popupContent);
+let greenIcon = L.icon({
+iconUrl: "icons/icon_culture.png",
+iconAnchor:   [50, 50],
+iconSize:     [100,100], // size of the icon
+popupAnchor:  [0,0] // point from which the popup should open relative to the iconAnchor
+});
+let redIcon = L.icon({
+iconUrl: "icons/icon_other.png",
+iconAnchor:   [50, 50],
+iconSize:     [100,100], // size of the icon
+popupAnchor:  [0,0] // point from which the popup should open relative to the iconAnchor
+});
+let yellowIcon = L.icon({
+iconUrl: "icons/icon_nationality.png",
+iconAnchor:   [50, 50],
+iconSize:     [100,100], // size of the icon
+popupAnchor:  [0,0] // point from which the popup should open relative to the iconAnchor
+});
+let blueIcon = L.icon({
+iconUrl: "icons/icon_august.png",
+iconAnchor:   [50, 50],
+iconSize:     [100,100], // size of the icon
+popupAnchor:  [0,0] // point from which the popup should open relative to the iconAnchor
+});
+
+    if(feature.category==="culture"){
+      whichIcon=greenIcon;
+      console.log("culture");
+    }
+    else if(feature.category==="august\'s life"){
+        whichIcon=blueIcon;
+    }
+    else if(feature.category==="nationality"){
+      whichIcon=yellowIcon;
+    }
+    else{
+      whichIcon=redIcon;
+    }
+    return L.marker(feature.latLng,{icon:whichIcon}).bindPopup(popupContent).on("click",markerOnClick);
+    function markerOnClick(e)
+{
+  if(category==="culture"){
+  $("#info").html("hi");
+}
+else if(category==="nationality"){
+$("#info").html("oh hi");
+}
+else if(category==="august\'s life"){
+$("#info").html("ahi");
+}
+}
+
+})
+);
+
+  // Add the layer to the map.
+  geoLayer.addTo(firstMap);
+  // Redraw the map so that all the markers are visible.
+  firstMap.fitBounds(geoLayer.getBounds());
+  // Zoom out one level to give some padding.
+  firstMap.zoomOut(1);
+});
