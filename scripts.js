6ae7353332af10f84acffead0ebefd7bb19ff373
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-07-04 23:33:15 -0400

    rename scripts oops.

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 //
 // This is a JavaScript file where you can type up your JavaScript
 // that will get processed by the file index.html, as you can see
-// on line 15 of index.html. Lines 12–14 of that file import the 
+// on line 15 of index.html. Lines 12–14 of that file import the
 // jQuery library (https://jquery.com/), which means you use
 // jQuery in this file.
 //
