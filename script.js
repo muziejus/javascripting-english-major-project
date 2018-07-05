4da0d15671b5b29a208287e11985eb9d434ad877
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-07-04 21:43:17 -0400

    Add script.js to initial project settings.

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js for The JavaScripting English Major
+//
+// This is a JavaScript file where you can type up your JavaScript
+// that will get processed by the file index.html, as you can see
+// on line 15 of index.html. Lines 12–14 of that file import the 
+// jQuery library (https://jquery.com/), which means you use
+// jQuery in this file.
+//
+// Because these lines begin with “//,” they are commented out,
+// that is, ignored by the browser. Hence, in order to rewrite the
+// contents of the “results-div,” you can uncomment the line below:
+
+// $("#results-div").html("Hello from scripts.js!");
