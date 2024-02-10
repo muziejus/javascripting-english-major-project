e62c99e0213b7569e1ec11c2642843883b1ed626
Amanda Dunker <93752687+amandadunker@users.noreply.github.com>
2024-02-09 20:30:36 -0500

    dinner question

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-alert("scripts.js has loaded!");
+let userInput;
+userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
+if ( userInput === "burrito" ) {
+    $("#response").html("Brilliant choice!");
+} else {
+    $("#response").html("Don't you want a burrito?");
+}
+
