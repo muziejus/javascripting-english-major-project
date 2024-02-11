969b5e51bf998edc747a42accdbe21d74408843c
Amanda Dunker <93752687+amandadunker@users.noreply.github.com>
2024-02-11 17:29:23 -0500

    Created modern word gloss

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Week 9: Building a Dataset
 
+
+Printing the first line
+
+~~~
+$("#prologue").html("<p>The text of the Prologue will go here.</p>");
+$("#glosses").html("<p>The glosses will go here.</p>");
+
+let line1, line1text;
+
+line1 = [{text: "Whan", modern: "When"}, {text: "that"}, {text: "Aprill,", modern: "April,"}, {text: "with"},
+{text: "his"}, {text: "shoures", modern: "showers"}, {text: "soote", modern: "sweet"}];
+//Added the property "modern" to the objects in the array line1
+
+line1text = line1.map(function(word) {
+    return word.text;
+}).join(" ");
+//.map creates a new array from line1's objects from thier "text" property
+//joining the "text" properties prints the first line
+
+$("#prologue").html("<p>" + line1text + "<br /></p>");
+~~~
--- prologue.js
+++ prologue.js
@@ -1,2 +1,32 @@
 $("#prologue").html("<p>The text of the Prologue will go here.</p>");
 $("#glosses").html("<p>The glosses will go here.</p>");
+
+let line1, line1text;
+line1 = [{text: "Whan", modern: "When"}, {text: "that"}, {text: "Aprill,", modern: "April,"}, {text: "with"},
+{text: "his"}, {text: "shoures", modern: "showers"}, {text: "soote", modern: "sweet"}];
+//line1 is an array of objects with two properties, "text" and "modern"
+
+//Create a blank string that opens two tags: 
+line1text = "<blockquote><p>";
+line1.forEach(function(word){
+    let wordString;
+    wordString = word.text;
+    if (word.modern){
+        wordString = `<a href='#' data-modern='${word.modern}'>${wordString}</a>`;
+    }
+    line1text = line1text + wordString + " ";
+});
+line1text = line1text + "<br /></p></blockquote>";
+
+$("#prologue").html(line1text);
+
+$("#prologue a").click(function(){
+    let glossText, clickedWord, modernWord;
+    clickedWord = $( this ).text();
+    modernWord = $ ( this ).data("modern");
+    glossText = "<h2>You clicked on: " + clickedWord + ", which means " + modernWord + "</h2>";
+    $("#glosses").html(glossText);
+});
+///the jQuery selector $("#prologue a") selects every <a> tag in #prologue
+//the click method tells it to execute a function when a user clicks
+//the function is created using the append method
